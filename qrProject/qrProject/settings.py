8748c26add48cefd6050dbaf8567a2910abcd918
imdadadelabou0 <imdadadelabou0@gmail.com>
2022-07-24 12:44:48 +0100

    [feat] Add logout middleware

--- qrProject/qrProject/settings.py
+++ qrProject/qrProject/settings.py
@@ -125,4 +125,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = "/" #Route to redirect user when he successful login in they account
+LOGIN_REDIRECT_URL = "home" #Route to redirect user when he successful login in they account
+LOGOUT_REDIRECT_URL = "home" #Route to redirect user when he Logout
