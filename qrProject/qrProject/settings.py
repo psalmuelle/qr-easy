81c7d53149ff724131a23648fc83362a74d9d0af
imdadadelabou0 <imdadadelabou0@gmail.com>
2022-07-24 11:15:25 +0100

    [feat] currently working on login middleware

--- qrProject/qrProject/settings.py
+++ qrProject/qrProject/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'qrProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "/" #Route to redirect user when he successful login in they account
